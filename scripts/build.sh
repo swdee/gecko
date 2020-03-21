878fdd21424952eaf0d85ea0d70a631ac2878d92
swdee <shanon@bokumail.com>
2020-03-22 12:17:05 +1300

    modify build script to correct path

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,14 +7,15 @@ PREFIX="${PREFIX:-$(pwd)/build}"
 SRC_DIR="$(dirname "${BASH_SOURCE[0]}")"
 source "$SRC_DIR/env.sh"
 
-GECKO_PKG=github.com/ava-labs/gecko
-GECKO_PATH="$GOPATH/src/$GECKO_PKG"
-if [[ -d "$GECKO_PATH/.git" ]]; then
-    cd "$GECKO_PATH"
-    go get -t -v "./..."
-    cd -
-else
-    go get -t -v "$GECKO_PKG/..."
-fi
+#GECKO_PKG=github.com/ava-labs/gecko
+#GECKO_PATH="$GOPATH/src/$GECKO_PKG"
+#if [[ -d "$GECKO_PATH/.git" ]]; then
+#    cd "$GECKO_PATH"
+#    go get -t -v "./..."
+#    cd -
+#else
+#    go get -t -v "$GECKO_PKG/..."
+#fi
+GECKO_PATH=`pwd`
 go build -o "$PREFIX/ava" "$GECKO_PATH/main/"*.go
 go build -o "$PREFIX/xputtest" "$GECKO_PATH/xputtest/"*.go
