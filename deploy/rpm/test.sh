1545b3e48791f741a867dc90f9480d0d128ddfd3
swdee <shanon@bokumail.com>
2020-03-25 17:23:25 +1300

    added all fedora builds

---
+++ deploy/rpm/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# script installs gecko on OS from YUM repository for integration testing
+
+# install repo
+yum -y install dnf-utils
+dnf config-manager --add-repo http://50.116.4.66:8080/yum/avalabs.repo
+
+if [ "${GIT_TAG}" == "" ]; then
+    # install nightly snapshot
+    dnf config-manager --set-enabled avalabs-fedora-unstable
+else
+    # tagged build
+    dnf config-manager --set-enabled avalabs-fedora
+fi
+
+# install and test
+yum -y install avalabs-gecko
+ava --help
+xputtest --help
