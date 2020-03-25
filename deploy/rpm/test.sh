ddbc2002c82a2755012115a82391451644d1820c
swdee <shanon@bokumail.com>
2020-03-25 18:20:12 +1300

    fix centos testing commands

--- deploy/rpm/test.sh
+++ deploy/rpm/test.sh
@@ -2,19 +2,47 @@
 #
 # script installs gecko on OS from YUM repository for integration testing
 
-# install repo
-yum -y install dnf-utils
-dnf config-manager --add-repo http://50.116.4.66:8080/yum/avalabs.repo
 
-if [ "${GIT_TAG}" == "" ]; then
-    # install nightly snapshot
-    dnf config-manager --set-enabled avalabs-${OS}-unstable
+install_centos() {
+    yum -y install yum-utils
+    yum-config-manager --add-repo http://50.116.4.66:8080/yum/avalabs.repo
+
+    if [ "${GIT_TAG}" == "" ]; then
+        # install nightly snapshot
+        yum-config-manager --enable avalabs-${OS}-unstable
+    else
+        # tagged build
+        yum-config-manager --enable avalabs-${OS}
+    fi
+
+    yum -y install avalabs-gecko
+}
+
+
+install_fedora() {
+    dnf -y install dnf-utils
+    dnf config-manager --add-repo http://50.116.4.66:8080/yum/avalabs.repo
+
+    if [ "${GIT_TAG}" == "" ]; then
+        # install nightly snapshot
+        dnf config-manager --set-enabled avalabs-${OS}-unstable
+    else
+        # tagged build
+        dnf config-manager --set-enabled avalabs-${OS}
+    fi
+
+    dnf -y install avalabs-gecko
+}
+
+
+# install gecko
+if [ "${OS}" == "centos" ]; then
+    install_centos
 else
-    # tagged build
-    dnf config-manager --set-enabled avalabs-${OS}
+    install_fedora
 fi
 
-# install and test
-yum -y install avalabs-gecko
+
+#  test
 ava --help
 xputtest --help
